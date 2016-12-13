d25ef9edca67b80d35d76a10524abda18599f8f7
David Eisner <david.eisner@oriel.oxon.org>
2016-12-13 10:28:33 +0000

    Slightly improve `gather-domains.sh` structure

--- letsencrypt/bin/gather-domains.sh
+++ letsencrypt/bin/gather-domains.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+scrape_nginx() {
+  find . -type f -print0|
+    xargs -0 grep -wh '^\s*server_name\b'|
+    grep -v '.well-known/acme-challenge'|
+    sed 's@.*://@@;s/;.*//'|
+    grep -o '\w[._[:alnum:]]*'|
+    grep -v '^\(_\|localhost\|server_name\)$'
+}
+
 find /servers -maxdepth 1 -type d|tail -n+2|while read d; do
-  find "$d" -type f -print0|xargs -0 grep -wh '^\s*server_name\b'|grep -v '.well-known/acme-challenge'|sed 's@.*://@@;s/;.*//'|grep -o '\w[._[:alnum:]]*'|grep -v '^\(_\|localhost\|server_name\)$'|sort -u|paste -sd,|grep -v ^$
+  (
+    cd "$d"
+    scrape_nginx
+  )|sort -u|paste -sd,|grep -v ^$
 done
