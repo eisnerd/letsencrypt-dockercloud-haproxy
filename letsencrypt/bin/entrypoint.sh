3e4d46742e281315e5eb473f920597d7fafa135e
David Eisner <david.eisner@oriel.oxon.org>
2016-12-14 15:38:49 +0000

    Revert "stash"
    This reverts commit de0d645aabec746afcd47aea95985c57b8bcb6e5.

--- letsencrypt/bin/entrypoint.sh
+++ letsencrypt/bin/entrypoint.sh
@@ -4,22 +4,22 @@
 [[ -z "$EMAIL" ]] && MISSING="$MISSING EMAIL"
 if [[ -n "$MISSING" ]]; then
 	echo "Missing required environment variables: $MISSING" >&2
-#	exit 1
+	exit 1
 fi
 
 # Wait for HAproxy to start before updating certificates on startup.
 # TODO: Use Dockerize, instead of assuming it takes 60 seconds to start.
 
-#mkdir -p /servers
+mkdir -p /servers
 
-#gather-domains.sh > domains
+gather-domains.sh > domains
 
 (
-#  sleep 15
-#  update-certs.sh
+  sleep 15
+  update-certs.sh
   max_starvation=60
   rm -f starved_since
-  inotifywait -mqre modify servers | while read EV; do
+  inotifywait -mqre modify /servers | while read EV; do
     starved_since="$(stat -c %Y starved_since 2> /dev/null)"
     if [ -n "$starved_since" ]; then
       [ $(($(date +%s) - $starved_since)) -gt $max_starvation ] &&
@@ -30,11 +30,9 @@ fi
     jobs -p | xargs kill -9 2> /dev/null
     (
       rm -f starved_since
-      sleep 5;
-      echo DONE
- #     new=$(mktemp)
- #     gather-domains.sh > $new
- #     mv $new domains.new && ! cmp domains domains.new && mv -v domains.new domains 2> /dev/null && cat -n domains >&2 && update-certs.sh
+      new=$(mktemp)
+      gather-domains.sh > $new
+      mv $new domains.new && ! cmp domains domains.new && mv -v domains.new domains 2> /dev/null && cat -n domains >&2 && update-certs.sh
     ) &
   done
 ) &
