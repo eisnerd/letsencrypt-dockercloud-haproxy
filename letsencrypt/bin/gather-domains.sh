ea0d4b8cc0f9abe7e05b7d0062e90952d8208ed9
David Eisner <david.eisner@oriel.oxon.org>
2017-11-07 12:45:10 +0000

    Include www. for wildcard fqdn

--- letsencrypt/bin/gather-domains.sh
+++ letsencrypt/bin/gather-domains.sh
@@ -4,16 +4,15 @@ scrape_nginx() {
   find . -type f -print0|
     xargs -0 grep -wh '^\s*server_name\b'|
     grep -v '.well-known/acme-challenge'|
-    sed 's@.*://@@;s/;.*//'|
-    grep -o '\w[._[:alnum:]]*'|
-    grep -v '^\(_\|localhost\|server_name\)$'
+    sed 's@.*://\|;.*\|\wlocalhost\w\|server_name _\?\|^[[:space:]]*@@g'|
+    sed 's/^\*\.\(.*\)/www.\1\n\1/'
 }
 
 find /servers -maxdepth 1 -type d|tail -n+2|while read d; do
   (
     cd "$d"
     scrape_nginx
-  )|sort -u|paste -sd,|grep -v ^$
+  )|sort -u|grep -v '^\s*$'|paste -sd,|grep -v ^$
 done
 
 exit 0
